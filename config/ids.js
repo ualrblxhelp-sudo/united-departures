7b286a8a1f0e1371f4f9eb7e57b7e3f610e616b6
ualrblxhelp-sudo <ualrblxhelp@gmail.com>
2026-02-11 19:01:15 -0500

    Update ids.js

--- config/ids.js
+++ config/ids.js
@@ -10,5 +10,6 @@ module.exports = {
     ARCHIVE_CHANNEL_ID: process.env.ARCHIVE_CHANNEL_ID,
     FLIGHT_HOST_ROLE_ID: process.env.FLIGHT_HOST_ROLE_ID,
     UNITED_TAIL_EMOJI: process.env.UNITED_TAIL_EMOJI || '✈️',
-    EMBED_COLOR: parseInt(process.env.EMBED_COLOR || '2596be', 16),
+    EMBED_COLOR: parseInt(process.env.EMBED_COLOR || '0b0fa8', 16),
+    STAFF_CALENDAR_CHANNEL_ID: process.env.STAFF_CALENDAR_CHANNEL_ID,
 };
